e13a492692db159642f3b282a80df1e840e6e40b
Siddhesh Gaykar <64803476+Siddhya007@users.noreply.github.com>
2020-12-02 12:59:50 +0530

    Upload Build

---
+++ precache-manifest.1a0d2196dd7a2bd90b4435da6eb1835b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b47b17585ea1593ef427118660924c",
+    "url": "/Siddhya007/tracker.git/index.html"
+  },
+  {
+    "revision": "a3bfabc4108b2eabd80e",
+    "url": "/Siddhya007/tracker.git/static/css/main.2d1d361c.chunk.css"
+  },
+  {
+    "revision": "843a8eebd61f68508b64",
+    "url": "/Siddhya007/tracker.git/static/js/2.b0975c60.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Siddhya007/tracker.git/static/js/2.b0975c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3bfabc4108b2eabd80e",
+    "url": "/Siddhya007/tracker.git/static/js/main.05454191.chunk.js"
+  },
+  {
+    "revision": "f5ff2cb10665c3d3ced1",
+    "url": "/Siddhya007/tracker.git/static/js/runtime-main.26f59fad.js"
+  },
+  {
+    "revision": "b33d4fb609ae6f541e26d10b084dcf0e",
+    "url": "/Siddhya007/tracker.git/static/media/image.b33d4fb6.png"
+  }
+]);
